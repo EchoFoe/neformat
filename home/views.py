48e48c2c75ae70c2e576bee4d8c0ae9d9e69ee72
EchoFoe <Aspirpd@gmail.com>
2019-11-05 01:22:37 +0300

    global commit

--- home/views.py
+++ home/views.py
@@ -228,6 +228,310 @@ def iPhone_11_pro(request):
 
     return render(request, 'iPhone_11_pro/iPhone_11_pro.html', locals())
 
+def iPhone_XS(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iphone_XS = all_products.filter(product__category__id=13)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPhone_XS/iPhone_XS.html', locals())
+
+def iPhone_XR(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iphone_XR = all_products.filter(product__category__id=14)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPhone_XR/iPhone_XR.html', locals())
+
+def iPhone_8(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iphone_8 = all_products.filter(product__category__id=15)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPhone_8/iPhone_8.html', locals())
+
+def iPhone_7(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iphone_7 = all_products.filter(product__category__id=16)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPhone_7/iPhone_7.html', locals())
+
+def apple_laptops(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_apple_laptops = all_products.filter(product__category__id=17)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'apple_laptops/apple_laptops.html', locals())
+
+def iMac(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iMac = all_products.filter(product__category__id=18)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iMac/iMac.html', locals())
+
+def watch(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'accessories/watch.html', locals())
+
+def watch_series_5(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_watch_series_5 = all_products.filter(product__category__id=19)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'watch_series_5/watch_series_5.html', locals())
+
+def watch_series_4(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_watch_series_4 = all_products.filter(product__category__id=20)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'watch_series_4/watch_series_4.html', locals())
+
+def watch_series_3(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_watch_series_3 = all_products.filter(product__category__id=21)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'watch_series_3/watch_series_3.html', locals())
+
+def watch_nike(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_watch_nike = all_products.filter(product__category__id=22)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'watch_nike/watch_nike.html', locals())
+
+def iPades(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPades/iPades.html', locals())
+
+def iPad(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iPad = all_products.filter(product__category__id=23)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPad/iPad.html', locals())
+
+def iPad_pro(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iPad_pro = all_products.filter(product__category__id=24)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPad_pro/iPad_pro.html', locals())
+
+def iPad_air(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iPad_air = all_products.filter(product__category__id=25)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPad_air/iPad_air.html', locals())
+
+def iPad_mini(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_iPad_mini = all_products.filter(product__category__id=26)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'iPad_mini/iPad_mini.html', locals())
+
+def quadrocopter(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_quadrocopter = all_products.filter(product__category__id=27)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'quadrocopter/quadrocopter.html', locals())
+
+def action_camera(request):
+
+    all_products = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+    new_products = all_products.filter(product__status__id=1)
+    products_action_camera = all_products.filter(product__category__id=28)
+
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        data = form.cleaned_data
+        print(form.cleaned_data["email"])
+        new_form = form.save()
+
+    return render(request, 'action_camera/action_camera.html', locals())
+
 def repairs(request):
 
     form = SubscriberForm(request.POST or None)
--- repairs/views.py
+++ repairs/views.py
@@ -6,6 +6,7 @@ def repairs(request):
     all_repairs = Repair.objects.filter(is_active=True)
 
     form = StatementForm(request.POST or None)
+    a = Form(request.POST or None)
 
     if request.method == "POST" and form.is_valid():
         print(request.POST)
@@ -13,5 +14,6 @@ def repairs(request):
         data = form.cleaned_data
         print(form.cleaned_data['name'])
         new_form = form.save()
+        new_a = a.save()
 
     return render(request, 'repairs/repairs.html', locals())
